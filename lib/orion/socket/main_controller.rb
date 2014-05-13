32f29182a1e55fb7df09576930de450e7af03cbd
Timothee Guerin <timothee.guerin@outlook.com>
2014-05-13 23:00:50 +0100

    more action

--- lib/orion/socket/main_controller.rb
+++ lib/orion/socket/main_controller.rb
@@ -2,7 +2,7 @@ module Orion
   module Socket
     class MainController < Orion::Socket::Controller
       get 'update_script_list' do
-
+        send_data('update_script_list', Orion::Engine.manager.scripts)
       end
     end
   end
